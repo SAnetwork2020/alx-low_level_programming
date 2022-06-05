572df2a93a04533b0642736e6c7ea021c3f5f9c0
Abdulganiyu Sulyman <suitpop2021@gmail.com>
2022-06-05 16:26:01 -0700

    auto-update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints to the standard out
+ *
+ * Return: return 0 to main function
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
