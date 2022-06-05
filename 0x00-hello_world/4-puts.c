f8b983b55f898800ec373b39d98262b7dc5deeb4
Abdulganiyu Sulyman <suitpop2021@gmail.com>
2022-06-05 16:05:07 -0700

    auto-update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main - This main description
+ * puts - Print the message or text
+ *
+ * Return: 0
  */
 int main(void)
 {
-  /**
-   * puts - This printout the text
-   */
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle")
 return (0);
 }
