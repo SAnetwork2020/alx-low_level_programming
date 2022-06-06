58b7722e2515930addb2cad05b0e03a9292196a7
Abdulganiyu Sulyman <suitpop2021@gmail.com>
2022-06-06 03:53:02 -0700

    auto-update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,8 @@ int main(void)
 {
 printf("%lu\n", sizeof(char));
 printf("%lu\n", sizeof(int));
-printf("%lu", sizeof(float));
-printf("%lu", sizeof(long int));
-printf("%lu", sizeof(long long int));
+printf("%lu\n", sizeof(float));
+printf("%lu\n", sizeof(long int));
+printf("%lu\n", sizeof(long long int));
 return (0);
 }
