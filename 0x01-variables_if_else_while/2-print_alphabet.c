04292050b037351a9c66ccceb3ee85a47212b0d3
Abdulganiyu Sulyman <suitpop2021@gmail.com>
2022-06-06 15:01:49 -0700

    auto-update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - This is the description
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+	putchar(ch);
+putchar("\n");
+getch();
+}
