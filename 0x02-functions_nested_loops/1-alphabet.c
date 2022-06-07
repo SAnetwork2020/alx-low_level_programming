b0ee1423388b82f67f7124472b837176a486a7a6
Abdulganiyu Sulyman <suitpop2021@gmail.com>
2022-06-07 13:19:49 -0700

    auto-update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,8 +4,15 @@
  *
  * Return: 0
  */
+#include "main.h"
+void print_alphabet(void)
+{
+for (int c = 97; c <= 122; ++c)
+putchar("%c ", c);
+putchar("\n");
+}
 int main(void)
 {
 print_alphabet();
-return (0);
+return 0;
 }
