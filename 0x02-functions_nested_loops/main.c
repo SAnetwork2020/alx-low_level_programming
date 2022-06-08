b590fb8cd630352518d37994be9ab7eb119ac364
Abdulganiyu Sulyman <suitpop2021@gmail.com>
2022-06-08 13:36:39 -0700

    auto-update

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,6 +1,11 @@
-int _putchar(int ch);
+int _putchar(int ch)
+{
+return (_putchar(ch));
+}
 void print_alphabet(void)
 {
-	for (char c = 'a'; c <= 'z'; c++)
-	_putchar(c);
+for (char c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar("\n");
+return (0);
 }
