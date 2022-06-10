ccc9ea016de2f6f84433c8db75d9c7d9fef240e5
Abdulganiyu Sulyman <suitpop2021@gmail.com>
2022-06-09 18:06:05 -0700

    auto-update

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+int main(){
+	int var1 = 'M';
+	int var2 = 'm';
+	int var3 = '3';
+	if (_isupper(var1) ){
+		printf("var1 = |%c| is uppercase character\n", var1 );
+	} else {
+		printf("var1 - |%c| is not uppercase character\n", var1 );
+	}
+
+	if (_isupper(var2) ) {
+		printf("var2 = |%c| is uppercase character\n", var2);
+	} else {
+		printf("var2 = |%c| is not uppercase character\n", var2);
+	}
+
+	if (_isupper(var3) ) {
+		printf("var3 = |%c| is uppercase character\n", var 3);
+	} else {
+		printf("var3 = |%c| is not uppercase character\n", var3);
+	}
+	return (0);
+}
+
+
+
+
+
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,3 @@
+int _putchar (int ch);
+int _isupper(int c);
+
