dfde1e8eb79bb1add1ee4d026668960196104d13
Abdulganiyu Sulyman <suitpop2021@gmail.com>
2022-06-10 21:02:42 -0700

    auto-update

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - tests function that prints if integer is positive or negatative
+ * Return: 0
+ */
+int main (void)
+{
+	int i = 98;
+	positive_or_negative(i);
+	return (0);
+}
