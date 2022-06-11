e9d01336fad2080468b4fa6b8fc6eb4996f8cd9c
Abdulganiyu Sulyman <suitpop2021@gmail.com>
2022-06-10 21:17:31 -0700

    auto-update

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,12 +6,12 @@
 int main(void)
 {
 	int i;
-	printf("Infinite loop incomming :(\n");
+	("Infinite loop incomming :(\n");
 	i = 0;
-	while (i < 10)
+	/*while (i < 10)
 	{
 		putchar(i);
-	}
+	}*/
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
