2a8af029980257cba900c618300658776305e03b
Abdulganiyu Sulyman <suitpop2021@gmail.com>
2022-06-10 23:20:29 -0700

    auto-update

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,20 @@
-
+/**
+ * _isalpha - checks whether is lowercase or uppercase alphabets
+ * @c: value to be taken by the function
+ *
+ * Return: isletter
+ */
 int _isalpha(int c)
+
 {
 	char lower, upper;
 	int isletter = 0;
+
 	for (lower = 'a'; lower <= 'z'; lower++)
 	{
 		for (upper = 'A'; upper <= 'Z'; upper++)
 		{
-			if ( c == lower || c == upper)
+			if (c == lower || c == upper)
 			{
 				isletter = 1;
 			}
