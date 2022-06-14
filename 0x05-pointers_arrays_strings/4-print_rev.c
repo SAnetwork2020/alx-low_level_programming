949d688dccdba44ae98cfb9570a95efc68500cad
Abdulganiyu Sulyman <suitpop2021@gmail.com>
2022-06-13 22:56:51 -0700

    auto-update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,9 +6,9 @@
 */
 void print_rev(char *s)
 {
-int len = 0, index;
-while (s[index++])
-len++;
+int len = 0, index = 0;
+len = _strlen(s);
+
 for (index = len - 1; index >= 0; index--)
 _putchar(s[index]);
 _putchar('\n');
