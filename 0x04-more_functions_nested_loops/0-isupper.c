9643f57ace0a01380acbefc2e85047191cddd331
Abdulganiyu Sulyman <suitpop2021@gmail.com>
2022-06-13 20:44:52 -0700

    Please

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,26 +1,25 @@
 #include "main.h"
-#include <stdio.h>
 #include <ctype.h>
-int main(){
+int _isupper(int ){
 	int var1 = 'M';
 	int var2 = 'm';
 	int var3 = '3';
-	if (_isupper(var1) ){
-		printf("var1 = |%c| is uppercase character\n", var1 );
+	if ( _isupper(var1) ){
+		_putchar (  "var1 = |%c| is uppercase  acter\n", var1 );
 	} else {
-		printf("var1 - |%c| is not uppercase character\n", var1 );
+		_putchar (  "var1 - |%c| is not uppercase  acter\n", var1 );
 	}
 
-	if (_isupper(var2) ) {
-		printf("var2 = |%c| is uppercase character\n", var2);
+	if ( _isupper(var2) ) {
+		_putchar (  "var2 = |%c| is uppercase  acter\n", var2);
 	} else {
-		printf("var2 = |%c| is not uppercase character\n", var2);
+		_putchar (  "var2 = |%c| is not uppercase  acter\n", var2);
 	}
 
-	if (_isupper(var3) ) {
-		printf("var3 = |%c| is uppercase character\n", var3);
+	if ( _isupper(var3) ) {
+		_putchar (  "var3 = |%c| is uppercase  acter\n", var3);
 	} else {
-		printf("var3 = |%c| is not uppercase character\n", var3);
+		_putchar (  "var3 = |%c| is not uppercase  acter\n", var3);
 	}
 	return (0);
 }
